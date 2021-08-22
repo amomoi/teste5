b0cda028bd0394fd37f2a0a9c1edb9600be53379
amomoi <momoi.alexandre@gmail.com>
2021-08-22 14:36:37 -0300

    path leituras post

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get 'home', to: 'home#index'
-  post 'api', to: 'sensors#create'
+  post 'api', to: 'leituras#create'
   get 'api', to: 'leituras#create'
 
 end
