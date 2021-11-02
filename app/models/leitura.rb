d879f81d9a2b60e1de367d59b08a7c038a0de1f4
amomoi <momoi.alexandre@gmail.com>
2021-11-02 17:18:30 -0300

    Envio SMS v2

--- app/models/leitura.rb
+++ app/models/leitura.rb
@@ -5,11 +5,7 @@ class Leitura < ApplicationRecord
     before_save :check_status
 
     def check_status
-
-      valor_anterior = Leitura.where("sensor_id = ?", sensor_id).select(:valor).last
-      p valor_anterior.valor
-      p valor
-      
+           
       limite_inferior = Sensor.where("id = ?", sensor_id).select(:Li).pluck(:Li)
       p "valor da VARIÁVEL"
       #limite_inferior.each do |limite_inferior|
@@ -21,14 +17,35 @@ class Leitura < ApplicationRecord
         p limite_superior[0]
       #end
 
+      valor_anterior = Leitura.where("sensor_id = ?", sensor_id).select(:valor).last
+        if valor_anterior = 'nil'
+          valor_anterior = limite_inferior[0] + 1
+        else 
+          p valor_anterior.valor
+          p valor
+        end
+        
       flag_notificacao = Status.where("sensor_id = ?", sensor_id).select(:flag).last
-      p flag_notificacao.flag
+        if flag_notificacao = 'nil'
+          flag_notificacao = 0
+        else 
+          p flag_notificacao.flag
+        end
 
       flag_rearme = Status.where("sensor_id = ?", sensor_id).select(:flag_rearme).last
-      p flag_rearme.flag_rearme
+        if flag_rearme = 'nil'
+          flag_rearme = 0
+        else 
+          p flag_rearme.flag_rearme
+        end
 
       flag_mantec = Status.where("sensor_id = ?", sensor_id).select(:flag_mantec).last
-      p flag_mantec.flag_mantec
+        if flag_mantec = 'nil'
+          flag_mantec = 0
+        else 
+          p flag_mantec.flag_mantec
+        end
+      
 
       #LÓGICA PARA SENSORES COM LI
       if flag_mantec = 0
@@ -37,8 +54,8 @@ class Leitura < ApplicationRecord
             p cliente[0]
             celular = Usuario.where("cliente_id = ?", cliente[0]).select(:celular)
             celular.each do |celular|
-              enviarSMS(celular.celular, cliente[0])
               p "Ativou SMS enviando para celular #{celular.celular} e cliente #{cliente[0]} " 
+              enviarSMS(celular.celular, cliente[0])
             end
           end
       else #flag_mantec = 1
@@ -53,5 +70,38 @@ class Leitura < ApplicationRecord
   public
   
   def enviarSMS(numero_celular, numero_cliente)
-    p "Enviou para #{numero_celular} do cliente #{numero_cliente}!"
+    p "Enviou SMS para #{numero_celular} e cliente numero #{numero_cliente}!"
+    # <%= let data = {
+    #             "smss": [
+    #                 {
+    #                     "numero": "#{numero_celular}",
+    #                     "idCustom": "numero_cliente",
+    #                     "mensagem": "Envio teste de SMS para Mainha - envia zap que recebeu este SMS"
+    #                 },
+    #                 # {
+    #                 #    "numero": "11995672927",
+    #                 #     "idCustom": "2",
+    #                 #    "mensagem": "Envio teste 2 de SMS para Paty"
+    #                 # },
+    #             ],
+    #             "envioImediato": true,
+    #             "centroCusto": "6141f787b62e99838c27e9dd",
+    #         };
+
+           
+    #         fetch('https://v2.bestuse.com.br/api/v1/envioApi?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6ImFtcG0iLCJfaWQiOiI2MTQxZjc4N2I2MmU5OTgzOGMyN2U5ZGQiLCJjbGllbnRlIjoiNjE0MWY3NjBiNjJlOTk4MzhjMjdlOWNmIiwic2FsZG8iOjE1LCJieUNDIjp0cnVlLCJpYXQiOjE2MzE3MjAzMjR9.3v4zB9VN4U18yf2syyH5djYnf_drkTViTfOV4EdSYcQ', {
+    #                 method: "POST",
+    #                 #mode: 'no-cors',
+    #                 body: JSON.stringify(data),
+    #                 headers: {
+    #                     'Accept': 'application/json',
+    #                     'Content-Type': 'application/json'
+    #                     },
+    #             })
+
+    #             # Converting to JSON
+    #             #.then(response => response.json())
+    #             # Displaying results to console
+    #             #.then(json => console.log(json));
+    #              %>
   end
